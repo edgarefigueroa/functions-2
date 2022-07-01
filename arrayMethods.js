62307efcd287d069107bc85acab234b83acc29a4
Edgar Figueroa <edgarefigueroa@outlook.com>
2022-07-01 15:45:44 -0400

    array  methods

--- arrayMethods.js
+++ arrayMethods.js
@@ -18,8 +18,9 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 // CODE HERE
-const evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
-
+//const evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+const evenNumbers = mixedNumbers.filter(number => number % 2 === 0);
+console.log(evenNumbers);
 
 
 ////////// PROBLEM 2 //////////
@@ -39,8 +40,9 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 // CODE HERE
-const postTaxPrices // = prices.map(/* Provide Your Callback Here );
-
+//const postTaxPrices // = prices.map(/* Provide Your Callback Here );
+const postTaxPrices = prices.map((price) => price * 1.07);
+console.log(postTaxPrices);
 
 
 ////////// PROBLEM 3 //////////
@@ -57,8 +59,9 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 // CODE HERE
-const totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
-
+//const totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+const totalPopulation = populations.reduce((acc, curr) => acc + curr);
+console.log(totalPopulation);
 
 
 ////////// PROBLEM 4 //////////
@@ -82,8 +85,9 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 // CODE HERE
-const myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
-
+//const myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+const myStrongest = monstersInYourPocket.filter( monster => monster.CP > 200)
+console.log(myStrongest);
 
 
 ////////// PROBLEM 5 //////////
@@ -100,6 +104,9 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
 */
 
 // CODE HERE
+const afterTaxSum = orders.map(order => order.price*(1+order.tax) )
+console.log(afterTaxSum);
+
 
 
 
@@ -120,3 +127,5 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
 */
 
 // CODE HERE
+const bobsTotal = purchases.filter(purchase => purchase.owner === 'Bob').reduce((acc, curr) => acc + curr.price,0)
+console.log(bobsTotal);
